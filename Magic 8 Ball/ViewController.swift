3d240020e0231263ec1b5f38cdcdef75fc6a7523
kevinseq1 <kevinseq1@gmail.com>
2022-12-26 18:25:13 -0800

    Update ViewController.swift and Main.storyboard 🛠

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    // Variable for the image view
+    @IBOutlet weak var ballImageView: UIImageView!
+        
 
-
-
+    // Changes the image to one of the images in the ballArray when the button is pressed
+    @IBAction func aButtonPressed(_ sender: UIButton) {
+        
+        // Array of all the images
+        let ballArray = [UIImage(imageLiteralResourceName: "ball1"), UIImage(imageLiteralResourceName: "ball2"), UIImage(imageLiteralResourceName: "ball3"), UIImage(imageLiteralResourceName: "ball4"), UIImage(imageLiteralResourceName: "ball5")]
+        
+        ballImageView.image = ballArray[Int.random(in: 0...4)]
+        
+    }
+    
+    
 }
 
